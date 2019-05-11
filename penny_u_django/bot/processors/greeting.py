98d81e53df41a4e0d0b6d9df9183b44c7cc8d306
John Berryman <jfberryman@gmail.com>
2019-05-11 06:16:35 +0000

    fixed urls

--- penny_u_django/bot/processors/greeting.py
+++ penny_u_django/bot/processors/greeting.py
@@ -44,8 +44,8 @@ class GreetingBotModule(BotModule):
 
 Penny University is a self-organizing, peer-to-peer learning community. In Penny U there are no "mentors" or "mentees" but rather peers that enjoy learning together, socially. 
 • If you have something you can teach, then let it be known. 
-• If you have something you want to learn, then reach out to the community or to an individual and ask for help - buy them a coffee of lunch, or jump on a Google Hangout. (This is called a Penny Chat.)
-• And when your meeting is complete, show your appreciation by posting a Penny Chat review in our <http://pennyuniversity.org|forum>. Teach us a little of what you have learned.
+• If you have something you want to learn, then reach out to the community or to an individual and ask for help - buy them a coffee or lunch, or jump on a Google Hangout. (This is called a Penny Chat.)
+• And when your Penny Chat is complete, show your appreciation by posting a Penny Chat review in our <http://pennyuniversity.org|forum>. Teach us a little of what you have learned.
 
 Penny U is on the move. If all goes well then I, your trusty robot sidekick, will gain super powers in the coming months. I will help you find the answers you're looking for. I will also replace our lowly <http://pennyuniversity.org|Google Groups home page> with something a little more... appealing. If you want to help use out then let <@U42HCBFEF> and <@UES202FV5> know.
 '''
