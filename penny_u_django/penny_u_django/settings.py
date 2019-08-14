a05a03a225b391fbdbdc968371ead3660f00653e
Nick Chouard <31971995+nchouard@users.noreply.github.com>
2019-08-13 21:54:28 -0500

    Added landing page to django app (#11)
    * Added landing page to django app
    
    * PEP* fix

--- penny_u_django/penny_u_django/settings.py
+++ penny_u_django/penny_u_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bot.apps.BotConfig',
+    'home'
 ]
 
 MIDDLEWARE = [
--- penny_u_django/penny_u_django/urls.py
+++ penny_u_django/penny_u_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', include('bot.urls')),
+    path('', include('home.urls'))
 ]
