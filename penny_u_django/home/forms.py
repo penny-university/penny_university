c58c045a2553c5bb5975713fc550507347469fba
Nick Chouard <nick.chouard@gmail.com>
2019-08-18 13:25:51 -0500

    Form styling updates

--- penny_u_django/home/forms.py
+++ penny_u_django/home/forms.py
@@ -2,6 +2,7 @@ from django import forms
 
 
 class InviteForm(forms.Form):
-    email = forms.CharField(label="Email", widget=forms.EmailInput(attrs={"class": "form-control"}))
+    email = forms.CharField(label="Email", widget=forms.EmailInput(attrs={"class": "form-control"}),
+                            help_text="We'll never share your email with anyone else.")
     how_did_you_find_us = forms.CharField(label="How'd You Find Us?",
-                                          widget=forms.TextInput(attrs={"class": "form-control"}))
+                                          widget=forms.TextInput(attrs={"class": "form-control"}), required=False)
