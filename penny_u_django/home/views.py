0ffc9ec5d87f9aace6719c430f01222f3369c4fd
Nick Chouard <31971995+nchouard@users.noreply.github.com>
2019-08-20 21:40:55 -0500

    Pop up form (#13)
    * Added form
    
    * Form styling updates
    
    * Adds requirements.txt
    
    * Adds requirements-test.txt
    
    * Adds CI workflow
    
    * Adds pytest-django
    
    * Delete requirements-test.txt
    
    * Delete requirements.txt
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Update ci.yml
    
    * Notify admins on form submission
    
    * Fixed styling
    
    * Adding GH actions for CI/CD (#12)
    
    Runs tests in python 3.6 and 3.7
    
    * Added form
    
    * Form styling updates
    
    * Notify admins on form submission
    
    * Fixed styling
    
    * Fixed styling

--- penny_u_django/home/views.py
+++ penny_u_django/home/views.py
@@ -1,7 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.conf import settings
+from .forms import InviteForm
+import slack
+
+from common.helpers import notify_admins
 
 # Create your views here.
+slack_client = slack.WebClient(token=settings.SLACK_API_KEY)
 
 
 def index(request):
-    return render(request, 'home/index.html')
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = InviteForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # send John and Nick a slack message with the person's information
+            email = form.cleaned_data["email"]
+            found_us = form.cleaned_data["how_did_you_find_us"]
+            message = f'Somebody just submitted a form!\nEmail: {email}\nHow They Found Us: {found_us}'
+            notify_admins(slack_client, message)
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thank-you/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = InviteForm()
+
+    return render(request, 'home/index.html', {'form': form})
+
+
+def thank_you(request):
+    return render(request, 'home/thank_you.html')
