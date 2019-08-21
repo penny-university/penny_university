12b718a5b702915d5439fb1984688acdddd5b380
Nick Chouard <nick.chouard@gmail.com>
2019-08-20 21:32:56 -0500

    Fixed styling

--- penny_u_django/home/forms.py
+++ penny_u_django/home/forms.py
@@ -2,13 +2,7 @@ from django import forms
 
 
 class InviteForm(forms.Form):
-    email = forms.CharField(
-        label="Email",
-        widget=forms.EmailInput(attrs={"class": "form-control"}),
-        help_text="We'll never share your email with anyone else."
-    )
-    how_did_you_find_us = forms.CharField(
-        label="How'd You Find Us?",
-        widget=forms.TextInput(attrs={"class": "form-control"}),
-        required=False
-    )
+    email = forms.CharField(label="Email", widget=forms.EmailInput(attrs={"class": "form-control"}),
+                            help_text="We'll never share your email with anyone else.")
+    how_did_you_find_us = forms.CharField(label="How'd You Find Us?",
+                                          widget=forms.TextInput(attrs={"class": "form-control"}), required=False)
