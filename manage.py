0e5561acc465183d510d2d15a4a78a69bce327bd
John Berryman <jnbrymn@github.com>
2019-09-07 22:16:51 -0500

    rearranged directory structure (#32)
    * rearranged directory structure
    
    * updated workflows
    
    * more workflow changes
    
    * more workflow changes
    
    * more workflow changes

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'penny_u_django.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'penny_university.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
