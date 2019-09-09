a703151084211aabece9ae503935e195cca20014
John Berryman <jnbrymn@github.com>
2019-09-08 22:04:13 -0500

    .

--- penny_university/settings.py
+++ penny_university/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
