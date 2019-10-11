05c3ed476e2e5015c50e318be62280f8cdfb255c
John Berryman <jnbrymn@github.com>
2019-10-10 23:02:58 -0500

    improved pytest in dev

--- conftest.py
+++ conftest.py
@@ -4,14 +4,8 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'penny_university.settings'
 
 
-try:
-    assert settings.SLACK_API_KEY == '' or settings.SLACK_API_KEY is None
-except AssertionError:
-    print(
-        'SLACK_API_KEY is set.\n'
-        'For safety reasons, tests are only run if settings.SLACK_API_KEY is set to empty string or None.\n'
-        'Try running with `SLACK_API_KEY= pytest`'
-    )
-    exit(1)
+if settings.SLACK_API_KEY not in ['', None] :
+    print('SLACK_API_KEY set to None for safety')
+    settings.SLACK_API_KEY=None
 
 os.environ['CONFTEST_HAS_BEEN_RUN'] = 'true'
