4d8c672c77c8b234fa86142c145a96122d1d09e0
Nick Chouard <nick.chouard@gmail.com>
2019-10-20 15:35:36 -0500

    Moved django_heroku to bottom of settings files

--- penny_university/settings.py
+++ penny_university/settings.py
@@ -14,9 +14,6 @@ import os
 import dj_database_url
 import django_heroku
 
-# Activate Django-Heroku. Suggested by Heroku for django configurations
-django_heroku.settings(locals())
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -163,3 +160,7 @@ SLACK_API_KEY = os.environ.get('SLACK_API_KEY')
 if SLACK_API_KEY is None:
     print('WARNING: SLACK_API_KEY is None')
 PENNY_ADMIN_USERS = ['@JB', '@nick.chouard']
+
+
+# Activate Django-Heroku. Suggested by Heroku for django configurations. Keep at the bottom of this file.
+django_heroku.settings(locals())
