6deee0069a5b3cb2b509c83d37ca9ca02239b8ed
John Berryman <jnbrymn@github.com>
2019-10-21 00:26:48 -0500

    protect exception

--- common/helpers.py
+++ common/helpers.py
@@ -2,5 +2,9 @@ from django.conf import settings
 
 
 def notify_admins(slack_client, message):
-    for user in settings.PENNY_ADMIN_USERS:
-        slack_client.chat_postMessage(channel=user, text=message)
+    try:
+        for user in settings.PENNY_ADMIN_USERS:
+            slack_client.chat_postMessage(channel=user, text=message)
+    except Exception:
+        # TODO log this
+        pass
