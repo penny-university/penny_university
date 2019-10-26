6ec43ca5ea9e4f55f67023bcf14d1c601df60185
Nick Chouard <nick.chouard@gmail.com>
2019-10-26 18:02:14 -0500

    Mocking out forum page

--- penny_university/urls.py
+++ penny_university/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', include('bot.urls')),
+    path('forum/', include('pennychat.urls')),
     path('', include('home.urls'))
 ]
