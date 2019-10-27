c0a3cae78b149f108b81c260b0af0c1cdaeac431
John Berryman <jnbrymn@github.com>
2019-10-26 23:05:25 -0500

    .

--- bot/tests/conftest_test.py
+++ bot/tests/conftest_test.py
@@ -4,7 +4,3 @@ import os
 def test_conftest_has_been_run():
     assert os.environ['CONFTEST_HAS_BEEN_RUN'] == 'true', \
         """It is important conftest.py be run because it checks that real keys are not defined during testing."""
-
-
-def test_trash():
-    assert True
