5975dfe73cbfa4db0a49d882efac668b9ef70d0f
John Berryman <jnbrymn@github.com>
2019-10-26 22:48:17 -0500

    working on flake

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ import os
 os.environ['DJANGO_SETTINGS_MODULE'] = 'penny_university.settings'
 
 
-if settings.SLACK_API_KEY not in ['', None] :
+if settings.SLACK_API_KEY not in ['', None]:
     print('SLACK_API_KEY set to None for safety')
-    settings.SLACK_API_KEY=None
+    settings.SLACK_API_KEY = None
 
 os.environ['CONFTEST_HAS_BEEN_RUN'] = 'true'
