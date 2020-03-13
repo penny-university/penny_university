5375ce81dbafe9fb35ef9049c4cfd38cf2f8fe54
John Berryman <jnbrymn@github.com>
2020-03-13 00:10:03 -0500

    shell hack

--- penny_university/management/commands/shell.py
+++ penny_university/management/commands/shell.py
@@ -5,7 +5,24 @@ class Command(ShellCommand):
     def ipython(self, options):
         print("REMINDER - THIS IS JOHN'S HACKED SHELL - EDIT IT TO GAIN SUPERPOWERS")
         from IPython import start_ipython
-        argv =  ['-c', 'a=3', '-i']
+
+        script_to_run_on_startup = """
+        from pennychat.models import PennyChat, PennyChatInvitation, FollowUp, Participant
+
+        from users.models import UserProfile
+
+        from slack import WebClient
+        from django.conf import settings
+        slack = WebClient(settings.SLACK_API_KEY)
+
+        expected_variables = ['WebClient']
+
+        expected_variables = expected_variables + ['In','Out','get_ipython','exit','quit', 'expected_variables']
+        available_variables = [k for k in locals().keys() if k[0] != '_' and k not in expected_variables]
+
+        print(f'\\navailable_variables:\\n{available_variables}')
+        """
+
+        argv = ['-c', script_to_run_on_startup, '-i']
 
         start_ipython(argv)
-
