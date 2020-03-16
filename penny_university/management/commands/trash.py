f62bd5919eb769eba5226b335a48c187e9d4186c
John Berryman <jnbrymn@github.com>
2020-03-15 21:34:35 -0500

    playing with heroku scheduling

--- penny_university/management/commands/trash.py
+++ penny_university/management/commands/trash.py
@@ -5,8 +5,4 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        i = 0
-        while True:
-            i += 1
-            print(i, 'XXXXXXXXX')
-            time.sleep(5.0)
+        print('YYYYYYYYY')
--- penny_university/settings/base.py
+++ penny_university/settings/base.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'background_task',
     'corsheaders',
     'rest_framework',
     'bot.apps.BotConfig',
