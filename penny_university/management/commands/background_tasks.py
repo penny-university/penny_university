a893911b13225ad3d3339873b50aa03c68787236
John Berryman <jnbrymn@github.com>
2020-04-11 15:43:45 -0500

    added more docs

--- penny_university/management/commands/background_tasks.py
+++ penny_university/management/commands/background_tasks.py
@@ -6,6 +6,11 @@ from bot.tasks import send_penny_chat_reminders
 
 
 class Command(ProcessTasks):
+    """This runs background tasks.
+
+    The background tasks either 1) run periodically, such as posting reminders for upcoming meetings or 2) run too slow
+    to be executed in the normal request cycle.
+    """
     def handle(self, *args, **options):
         send_penny_chat_reminders()
         # TODO this is a good place to update events and mark them as completed
@@ -13,7 +18,10 @@ class Command(ProcessTasks):
 
 
 def run_process_tasks():
-    """Runs background tasks such as those in bot/tasks/pennychat"""
+    """Runs background tasks such as those in bot/tasks/pennychat
+
+    See the document for how this works here https://django-background-tasks.readthedocs.io/en/latest/
+    """
     process_tasks = ProcessTasks()
     process_tasks.sig_manager = SignalManager()
     process_tasks.run(duration=settings.PROCESS_TASKS_DURATION_SECONDS)
