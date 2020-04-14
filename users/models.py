70f187c6d2a16c2a834026a11574de121ddd1164
Nick Chouard <nick.chouard@gmail.com>
2020-04-14 18:52:57 -0500

    Changed 'user' table to 'users.user_profile', which is the default

--- users/models.py
+++ users/models.py
@@ -28,9 +28,6 @@ class UserProfile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        db_table = 'user'
-
     def __repr__(self):
         return pprint_obj(self)
 
