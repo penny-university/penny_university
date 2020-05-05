65db4b0218c67e408b97bb81df4144646da08079
coreyar <coreyarice@gmail.com>
2020-05-05 00:46:06 -0300

    Sketch out some permissions gating

--- penny_university_frontend/src/selectors/user.js
+++ penny_university_frontend/src/selectors/user.js
@@ -5,7 +5,7 @@ const getToken = (store) => getUserStore(store).token
 
 const getAuthed = (store) => !!getUserStore(store).user
 
-const getUser = (store) => !!getUserStore(store).user
+const getUser = (store) => getUserStore(store).user
 
 export {
   getToken,
