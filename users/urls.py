c194a76e4162640f0c160388ac166c0df0e5492d
coreyar <coreyarice@gmail.com>
2020-05-06 00:07:37 -0300

    remove scratch code

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
 from .views import (
     UserProfileDetail,
-    UserProfileList,
     ListUserChats,
 )
 
 user_profile_detail = UserProfileDetail.as_view()
-user_profile_list = UserProfileList.as_view()
 user_chats_list = ListUserChats.as_view()
 
 urlpatterns = [
     path('<int:pk>/chats/', user_chats_list, name='user-chat-list'),
-    path('profile/<int:pk>/', user_profile_detail, name='userprofile-detail'),
-    path('profile/', user_profile_list, name='userprofile-list'),
+    path('<int:pk>/', user_profile_detail, name='userprofile-detail'),
 ]
