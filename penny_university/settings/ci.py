e5fefd4a7ca76779b56665f6921c4efa20f86720
John Berryman <jnbrymn@github.com>
2020-05-19 22:48:26 -0500

    rm python 3.7 from workflow

--- penny_university/settings/ci.py
+++ penny_university/settings/ci.py
@@ -1,13 +1,3 @@
-# from penny_university.settings.base import *
-#
-# # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-#
-# TASK_ALWAYS_EAGER = True
-
-
-import os
-
 from penny_university.settings.base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
