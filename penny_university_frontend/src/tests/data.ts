c92835d80a44a07fd521174242ea9817789800a1
coreyar <coreyarice@gmail.com>
2020-05-21 00:02:03 -0300

    add tests

--- penny_university_frontend/src/tests/data.ts
+++ penny_university_frontend/src/tests/data.ts
@@ -3,16 +3,19 @@ import { camelizeKeys } from 'humps'
 import chatDetail from '../../../__mocks__/api-chats-get-1-200.json'
 import chatList from '../../../__mocks__/api-chats-get-200.json'
 import followUpsList from '../../../__mocks__/api-chats-get-1-follow-ups-200.json'
-import { User } from '../models'
+
 import { Schemas } from '../models/schemas'
 
+// Chats
 export const chats = camelizeKeys(chatList.results.slice(2,3))
-export const chatsNext = camelizeKeys(chatList.results.slice(0,1))
-export const followUps = camelizeKeys(followUpsList)
-
 export const { chats: normalizedChats, users } = normalize(chats, Schemas.CHAT_ARRAY).entities
-
+// Mocked Chat paging
+export const chatsNext = camelizeKeys(chatList.results.slice(0,1))
 export const { chats: normalizedNextChats } = normalize(chatsNext, Schemas.CHAT_ARRAY).entities
+// Single Chat
+export const chat = camelizeKeys(chatDetail)
+export const { chats: normalizedChat } = normalize(chat, Schemas.CHAT).entities
 
+export const followUps = camelizeKeys(followUpsList)
 
 export const { followUps: normalizedFollowUps}  = normalize(followUps, Schemas.FOLLOW_UP_ARRAY).entities
