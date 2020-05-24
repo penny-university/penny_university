3ea0f8c6de38e9af699632d8b44dcd4fa6df4c51
coreyar <coreyarice@gmail.com>
2020-05-24 18:31:47 -0300

    add test

--- pennychat/tests/api/test_penny_chat.py
+++ pennychat/tests/api/test_penny_chat.py
@@ -24,6 +24,16 @@ def test_penny_chat_list(test_chats_1):
     assert response.data['results'][0]['participants'][0]['user']['email'] == 'three@wherever.com'
     assert chats[0]['title'] == most_recent_chat.title
 
+@pytest.mark.django_db
+def test_penny_chat_list(test_chats_1):
+    client = APIClient()
+    response = client.get('/api/chats/?participants__user_id=1')
+    assert response.status_code == 200
+    assert response.data['count'] == 2
+    chats = response.data['results']
+    for chat in chats:
+        assert 1 in [participant['user']['id'] for participant in chat['participants']]
+
 
 @pytest.mark.django_db
 def test_penny_chat_detail(test_chats_1):
