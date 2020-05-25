1955ecf774f5b877790feafd753b197a56ad7a38
coreyar <coreyarice@gmail.com>
2020-05-25 12:12:05 -0300

    fix test

--- pennychat/tests/api/test_penny_chat.py
+++ pennychat/tests/api/test_penny_chat.py
@@ -28,12 +28,13 @@ def test_penny_chat_list(test_chats_1):
 @pytest.mark.django_db
 def test_penny_chat_participants_list(test_chats_1):
     client = APIClient()
-    response = client.get('/api/chats/?participants__user_id=1')
+    user_id = test_chats_1[0].participants.all()[0].user_id
+    response = client.get(f'/api/chats/?participants__user_id={user_id}')
     assert response.status_code == 200
     assert response.data['count'] == 2
     chats = response.data['results']
     for chat in chats:
-        assert 1 in [participant['user']['id'] for participant in chat['participants']]
+        assert int(user_id) in [participant['user']['id'] for participant in chat['participants']]
 
 
 @pytest.mark.django_db
