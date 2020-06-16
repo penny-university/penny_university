19e9ba29913954b3300fec1b8273e81c62159ed1
Nick Chouard <nick.chouard@gmail.com>
2020-06-15 22:26:38 -0500

    PR updates

--- conftest.py
+++ conftest.py
@@ -2,8 +2,11 @@ from django.conf import settings
 import os
 
 if settings.SLACK_API_KEY not in ['', None]:
-    print('SLACK_API_KEY and EMAIL_HOST_PASSWORD have been set to None for safety')
+    print('SLACK_API_KEY has been set to None for safety')
     settings.SLACK_API_KEY = None
+
+if settings.EMAIL_HOST_PASSWORD not in ['', None]:
+    print('EMAIL_HOST_PASSWORD has been set to None for safety')
     settings.EMAIL_HOST_PASSWORD = None
 
 os.environ['CONFTEST_HAS_BEEN_RUN'] = 'true'
