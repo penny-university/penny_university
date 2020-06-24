f30438afe68aaaf8fac3d1270b11ebe96e7a9376
John Berryman <jnbrymn@github.com>
2020-06-23 20:58:48 -0500

    .

--- pennychat/tests/api/test_follow_up.py
+++ pennychat/tests/api/test_follow_up.py
@@ -1,3 +1,5 @@
+import logging
+
 import pytest
 
 from rest_framework.test import APIClient
@@ -19,10 +21,11 @@ def test_follow_up_list(test_chats_1):
     assert follow_ups[0]['content'] == 'The first follow up'
     assert FollowUp.objects.get(pk=follow_ups[0]['id']).content == 'The first follow up'
 
-
+import sys
 @pytest.mark.django_db
-def test_create_follow_up(test_chats_2):
-    penny_chat = test_chats_2[0]
+def test_create_follow_up(test_chats_1):
+    penny_chat = test_chats_1[0]
+    print(f"\nALL FOLLOWUPS:\n{penny_chat.follow_ups.all()}\n")
     user = penny_chat.get_organizer()
     token = Token.objects.create(user=user)
     client = APIClient()
@@ -35,10 +38,10 @@ def test_create_follow_up(test_chats_2):
     assert response.status_code == 201
     response = client.get(f'/api/chats/{penny_chat.id}/follow-ups/')
     follow_ups = response.data
+    print(f"\nFOLLOW_UPS:\n{follow_ups}\n")
     assert len(follow_ups) == 3
     assert follow_ups[2]['content'] == content
     assert FollowUp.objects.get(pk=follow_ups[2]['id']).content == content
-
 
 @pytest.mark.django_db
 def test_create_follow_up_unauthorized(test_chats_1):
