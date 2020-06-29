6cdc01a217cc67053374415eb998ad7d60c47bab
John Berryman <jnbrymn@github.com>
2020-06-28 22:59:21 -0500

    save point

---
+++ users/management/commands/welcome_back_email.py
@@ -0,0 +1,51 @@
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand
+from django.db.models import Count
+from django.template.loader import render_to_string
+
+from users.models import User
+
+
+class Command(BaseCommand):
+    help = (
+        'Email everyone associate with Penny U and tell them that we have a new website.'
+    )
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--live_run',
+            dest='live_run',
+            action='store_true',
+            help='opposite of dry run - will actually send emails',
+            required=False,
+        )
+
+        parser.add_argument(
+            '--other_emails',
+            dest='other_emails',
+            help='csv list of emails that we should send a generic email to.',
+            required=True,
+        )
+
+    def handle(self, *args, **kwargs):
+        users = User.objects.annotate(chat_count=Count('user_chats'))
+        for user in users:
+            send_welcome_back_email(user, live_run=False)
+
+
+def send_welcome_back_email(user, live_run=False):
+    context = {
+        # 'first_name': user.first_name
+    }
+    text_email = render_to_string('users/welcome_back_email.txt', context)
+    html_email = render_to_string('users/welcome_back_email.html', context)
+    if live_run:
+        send_mail(
+            subject='Greeting from Penny University | Take a look at what we\'ve been up to!',
+            message=text_email,
+            from_email='"Penny University" <lincoln@pennyuniversity.org>',
+            recipient_list=user.email,
+            html_message=html_email,
+        )
+    else:
+        print(f'USER: {user.first_name} {user.last_name}\n\nMESSAGE: {text_email}\n')
