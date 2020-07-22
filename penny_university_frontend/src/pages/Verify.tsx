c594987a6451a80e69825b3f0a11ac92d573bc04
Nick Chouard <nick.chouard@gmail.com>
2020-07-22 15:50:13 -0600

    Add link to go back to chats page

--- penny_university_frontend/src/pages/Verify.tsx
+++ penny_university_frontend/src/pages/Verify.tsx
@@ -2,10 +2,12 @@ import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { ThunkDispatch } from 'redux-thunk'
 import { AnyAction } from 'redux'
-import { RouteComponentProps } from 'react-router-dom'
+import { RouteComponentProps, Link } from 'react-router-dom'
 import queryString from 'query-string'
 import { RootState } from '../reducers'
 import { verifyEmail } from '../actions/user'
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronLeft} from "@fortawesome/free-solid-svg-icons";
 
 type StateProps = {
   error: string | null,
@@ -28,6 +30,7 @@ const VerifyPage = ({ verify, location, error }: VerifyPageProps) => {
   return (
     <div>
       <h1 className="text-center">{message}</h1>
+      <h4 className="mt-4 text-center"><Link to='/chats'><FontAwesomeIcon icon={faChevronLeft} /> Go Back to Chats</Link></h4>
     </div>
   )
 }
