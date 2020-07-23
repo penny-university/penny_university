b33899996504471f71369d310055e3850e5edf1c
Nick Chouard <nick.chouard@gmail.com>
2020-07-23 10:37:07 -0600

    Lint

--- penny_university_frontend/src/pages/Verify.tsx
+++ penny_university_frontend/src/pages/Verify.tsx
@@ -4,10 +4,10 @@ import { ThunkDispatch } from 'redux-thunk'
 import { AnyAction } from 'redux'
 import { RouteComponentProps, Link } from 'react-router-dom'
 import queryString from 'query-string'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
 import { RootState } from '../reducers'
 import { verifyEmail } from '../actions/user'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronLeft} from "@fortawesome/free-solid-svg-icons";
 
 type StateProps = {
   error: string | null,
@@ -31,7 +31,13 @@ const VerifyPage = ({ verify, location, error }: VerifyPageProps) => {
   return (
     <div>
       <h1 className="text-center">{message}</h1>
-      <h4 className="mt-4 text-center"><Link to='/chats'><FontAwesomeIcon icon={faChevronLeft} /> Go Back to Chats</Link></h4>
+      <h4 className="mt-4 text-center">
+        <Link to="/chats">
+          <FontAwesomeIcon icon={faChevronLeft} />
+          {' '}
+          Go Back to Chats
+        </Link>
+      </h4>
     </div>
   )
 }
