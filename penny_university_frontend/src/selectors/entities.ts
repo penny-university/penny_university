51d999428e275fac9c3211c5db290cd54d3cb9f5
Corey Rice <7258308+coreyar@users.noreply.github.com>
2020-09-01 23:57:42 -0300

    bug-135-first-followup-crash (#346)
    * revert saving on login/signup
    
    * Fix case statement, clarify function name
    
    * fix test
    
    * eslint
    
    * add missing fixtures
    
    * flake8
    
    * remove irrelevant test
    
    * Fallback to user on first followup if entity cant be found

--- penny_university_frontend/src/selectors/entities.ts
+++ penny_university_frontend/src/selectors/entities.ts
@@ -1,4 +1,7 @@
 import { RootState } from '../reducers'
+import { User } from '../models'
+import { getUser } from './user'
+
 
 const getChats = (state: RootState) => state.entities.chats
 
@@ -6,7 +9,18 @@ const getFollowUps = (state: RootState) => state.entities.followUps
 
 const getUsers = (state: RootState) => state.entities.users
 
-const getUserByID = (state: RootState, id: string) => getUsers(state)[id]
+const getUserByID = (state: RootState, id: string) => {
+  let account: Account | User = getUsers(state)[id]
+  // Until the logged in user creates their first follow up they won't get normalized as an entity
+  // In this case we fallback to the user
+  if (!account) {
+    const user = getUser(state)
+    if (user.id.toString() === id.toString()) {
+      account = user
+    }
+  }
+  return account
+}
 
 export {
   getChats,
