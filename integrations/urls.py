35f82bd1fe37ed92daedbbb6dfe06a397e783ee2
Nick Chouard <nick.chouard@gmail.com>
2020-11-15 11:54:58 -0600

    Fix urls

--- integrations/urls.py
+++ integrations/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('google/auth-request/', views.auth_request, name='google-auth-request'),
-    path('google/auth-success/', views.auth_success, name='google-auth-success'),
-    path('google/calendar/', views.create_calendar_event, name='google-calendar'),
+    path('google/auth-success/', views.auth_success, name='google-auth-success')
 ]
