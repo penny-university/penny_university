ade86535940c296810ec38edfae4220dd66b6990
Colin Pokowitz <cp@cpdev.me>
2020-12-01 21:29:20 -0600

    fix chat ordering bug pt. 2

--- penny_university_frontend/src/pages/Chats.tsx
+++ penny_university_frontend/src/pages/Chats.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
-import { ChatList } from '../components'
+import React, { useEffect } from 'react'
+import { AnyAction } from 'redux'
+import { ThunkDispatch } from 'redux-thunk'
+import { connect } from 'react-redux'
+import { RouteComponentProps } from 'react-router-dom'
+import ChatList from '../components/chats/ChatList'
+import { loadChatsList } from '../actions/chat'
+import ApiRoutes from '../constants'
 
-const ChatsPage = () => (
-  <ChatList filter={{ key: 'all', query: '' }} />
-)
+type DispatchProps = {
+  loadChatsList: (nextPageUrl?: string, userID?: string) => void,
+}
 
-export default ChatsPage
+type TParams = { id: string };
+
+type ChatsProps = DispatchProps & RouteComponentProps<TParams>
+
+const Chats = ({
+  loadChatsList, match,
+}: ChatsProps) => {
+  useEffect(() => {
+    loadChatsList(ApiRoutes.chats, 'all')
+  }, [loadChatsList])
+  return (
+    <div>
+      <ChatList filter={{ key: 'all', query: '' }} />
+    </div>
+  )
+}
+
+const mapDispatchToProps = (dispatch: ThunkDispatch<unknown, unknown, AnyAction>) => ({
+  loadChatsList: (nextPageUrl: string, userID: string) => dispatch(loadChatsList(nextPageUrl, userID)),
+})
+
+// @ts-ignore
+export default connect(null, mapDispatchToProps, null)(Chats)
