cf693bd714842081a643ee92e9212fa15b8060a1
John Berryman <jnbrymn@github.com>
2020-12-13 23:27:08 -0600

    documentation

--- matchmaking/management/commands/initialize_automated_matching.py
+++ matchmaking/management/commands/initialize_automated_matching.py
@@ -9,12 +9,13 @@ class Command(BaseCommand):
     University, but since we don't know how we plan to do that yet, this is a starting point.
 
     The functionality is as follows.
-    * First a recurring event is set up that will `request_matches` for all TopicChannels every 15 days. Here we'll
-        encourage people to sign up to be matched.
-    * Next, as part of the request match run, a task will be set up 7 days later to `make_matches`. Here we'll pair up
-        the people that requested to be matched, and send them a private message to encourage them to schedule a chat.
-    * Next, as part of the make_matches run, another task will be set up 7 days later to `remind_matches`. Here we find
-        the pairs that haven't yet set up a match and we'll encourage them to set one up.
+    * First a recurring event is set up that will `request_matches` for all TopicChannels every PERIOD_IN_DAYS(15) days.
+        Here we'll encourage people to sign up to be matched.
+    * Next, as part of the request match run, a task will be set up DAYS_AFTER_REQUEST_TO_MAKE_MATCH(7) days later to
+        `make_matches`. Here we'll pair up the people that requested to be matched, and send them a private message to
+        encourage them to schedule a chat.
+    * Next, as part of the make_matches run, another task will be set up DAYS_AFTER_MATCH_TO_REMIND(7) days later to
+        `remind_matches`. Here we find the pairs that haven't yet set up a match and we'll encourage them to set one up.
     """
 
     def add_arguments(self, parser):
